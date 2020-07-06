294ee10e2993b41602ba7f6f9d19bdef0612732b
1 <1>
2020-07-06 19:56:08 +0800

    feat: HW

--- assignment/HelloWorld/app/src/main/java/com/bytedance/helloworld/MainActivity.java
+++ assignment/HelloWorld/app/src/main/java/com/bytedance/helloworld/MainActivity.java
@@ -3,13 +3,27 @@ package com.bytedance.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private Button btn;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Maybe you are right.");
+            }
+        });
         Log.d(TAG, "MainActivity");
     }
 }
